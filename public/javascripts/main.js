feadbe6a6281216807b67a89eb426647df67534e
pescado-muerto <el.vientre.santo@gmail.com>
2013-11-23 04:55:48 -0200

    several changes

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,22 +6,22 @@ map.doubleClickZoom.disable();
 map.scrollWheelZoom.disable();
 
 $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-        var target = $(this.hash);
+  if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+    var target = $(this.hash);
 
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 
-        if (target.length) {
-            $('html,body').animate({
-                scrollTop: target.offset().top
-            }, 1000);
-            return false;
-        }
+    if (target.length) {
+      $('html,body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
     }
+  }
 });
 
-$('.row:not(#map)').each(function() {
-    var prevElemHeight = $(this).prev().height();
+$('.row:not(#map,#portada)').each(function() {
+  var prevElemHeight = $(this).prev().height();
 
-    $(this).css({top: prevElemHeight + 'px'});
+  $(this).css({top: prevElemHeight + 'px'});
 });
