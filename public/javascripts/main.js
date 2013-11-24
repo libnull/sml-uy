332cc5c2a3de622c017df5978019abeac1d96aa7
pescado-muerto <el.vientre.santo@gmail.com>
2013-11-24 15:26:51 -0200

    contact added + styles fixed

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -20,7 +20,7 @@ $('a[href*=#]:not([href=#])').click(function() {
   }
 });
 
-$('.row:not(#portada)').each(function() {
+$('.row:not(#portada,#map)').each(function() {
   var prevElemHeight = $(this).prev().height();
 
   $(this).css({top: prevElemHeight + 'px'});
